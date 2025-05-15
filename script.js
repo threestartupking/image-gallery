bc44544d60033369aa0afbc97a1fb76f90cde1d8
threestartupking <three697184@gmail.com>
2025-05-15 12:15:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const data = [
-    { img: "images/img1.jpg", text: "This is image 1" },
-    { img: "images/img2.jpg", text: "This is image 2" },
-    { img: "images/img3.jpg", text: "This is image 3" },
-    { img: "images/img4.jpg", text: "This is image 4" },
-    { img: "images/img5.jpg", text: "This is image 5" },
-    { img: "images/img6.jpg", text: "This is image 6" },
-    { img: "images/img7.jpg", text: "This is image 7" },
-    { img: "images/img8.jpg", text: "This is image 8" },
-    { img: "images/img9.jpg", text: "This is image 9" },
-    { img: "images/img10.jpg", text: "This is image 10" },
-    { img: "images/img11.jpg", text: "This is image 11" },
-    { img: "images/img12.jpg", text: "This is image 12" },
+    { img: "images/img1.webp", text: "This is image 1" },
+    { img: "images/img2.webp", text: "This is image 2" },
+    { img: "images/img3.webp", text: "This is image 3" },
+    { img: "images/img4.webp", text: "This is image 4" },
+    { img: "images/img5.webp", text: "This is image 5" },
+    { img: "images/img6.webp", text: "This is image 6" },
+    { img: "images/img7.webp", text: "This is image 7" },
+    { img: "images/img8.webp", text: "This is image 8" },
+    { img: "images/img9.webp", text: "This is image 9" },
+    { img: "images/img10.webp", text: "This is image 10" },
+    
     // Add up to 100 items
   ];
   
